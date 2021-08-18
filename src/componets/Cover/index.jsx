5a7078646f04386d0e33edf0320d663c5789beb0
Nirvana-30 <1357711537@qq.com>
2021-08-18 15:53:12 +0800

    文本及样式

--- src/componets/Cover/index.jsx
+++ src/componets/Cover/index.jsx
@@ -16,6 +16,7 @@ export default class Cover extends Component {
         <div>探索房间</div>
         <div>发现新生要素</div>
         <div>点击特定元素，触发未知之旅</div>
+        <div>解锁新生四要素，收获专属邀请函</div>
         <div>自由滑动，探索更多</div>
         <div className="leftArrow"></div>
         <div className="rightArrow"></div>
